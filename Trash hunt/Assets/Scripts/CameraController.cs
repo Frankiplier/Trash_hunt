2166648c11f2be16a4f780f0e229fb9a180cfc89
Frankiplier <agatarucka@gmail.com>
2024-03-21 12:14:47 +0100

    Added simple scene, movement and camera controlls

---
+++ Trash hunt/Assets/Scripts/CameraController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;
+    public float offset;
+    public float offsetSmoothing;
+    private Vector3 targetPosition;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //locking camera on a target
+        targetPosition = new Vector3(target.position.x, transform.position.y, transform.position.z);
+
+        //ify na smooth przejscia kamera
+        if(target.localScale.x > 0)
+        {
+            targetPosition = new Vector3(targetPosition.x + offset, targetPosition.y, targetPosition.z);
+        }
+        else
+        {
+            targetPosition = new Vector3(targetPosition.x - offset, targetPosition.y, targetPosition.z);
+        }
+
+        //kod na czas reakcji kamery
+        transform.position = Vector3.Lerp(transform.position, targetPosition, offsetSmoothing * Time.deltaTime);
+    }
+
+}
---
+++ Trash hunt/Assets/Scripts/MovementController.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerMovementScript : MonoBehaviour
+{
+    public Rigidbody2D player;
+    public float speed;
+    private float direction = 0;
+
+    private Vector3 respawnPoint;
+    public Transform fallDetector;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GetComponent<Rigidbody2D>();
+        respawnPoint = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // definicja kierunku i zapisywanie w logu
+        direction = Input.GetAxis("Horizontal");
+        Debug.Log(direction);
+
+        // ify na sterowanie postacia lewo-prawo
+        if (direction < 0)  
+        {
+            player.velocity = new Vector2(direction * speed, player.velocity.y);
+            transform.localScale = new Vector2(-0.8f, 0.8f);
+        }
+        else if (direction > 0)
+        {
+            player.velocity = new Vector2(direction * speed, player.velocity.y);
+            transform.localScale = new Vector2(0.8f, 0.8f);
+        }
+
+        else
+        {
+            player.velocity = new Vector2(0, player.velocity.y);
+        }
+
+        // kod na poruszanie sie triggera wraz z postacia
+        fallDetector.transform.position = new Vector2(transform.position.x, fallDetector.transform.position.y);
+        }
+
+        // ify na trigger od spandania i zbieranie nasionek
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.tag == "FallDetector")
+            {
+                transform.position = respawnPoint;
+            }
+    }
+}
