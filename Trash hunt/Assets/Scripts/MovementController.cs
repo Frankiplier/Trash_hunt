d4568a814c79386146439ebaad943db9e65ee331
Frankiplier <agatarucka@gmail.com>
2024-03-24 17:49:21 +0100

    Cosmetic change

--- Trash hunt/Assets/Scripts/MovementController.cs
+++ Trash hunt/Assets/Scripts/MovementController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class PlayerMovementScript : MonoBehaviour
+public class MovementController : MonoBehaviour
 {
     [SerializeField] public GameObject icon;
     public bool IsDisplayed = false;
