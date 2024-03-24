c9e22b256c319586739950e3c5cf81ea2f97c90f
Frankiplier <agatarucka@gmail.com>
2024-03-24 15:59:00 +0100

    Added dash

--- Trash hunt/Assets/Scripts/MovementController.cs
+++ Trash hunt/Assets/Scripts/MovementController.cs
@@ -5,12 +5,21 @@ using UnityEngine.UI;
 
 public class PlayerMovementScript : MonoBehaviour
 {
+    // normal movement 
     public Rigidbody2D player;
     public float speed;
     private float direction = 0;
 
+    // respawn point and fall detector
     private Vector3 respawnPoint;
     public Transform fallDetector;
+
+    // dashing
+    private bool canDash = true;
+    private bool isDashing;
+    private float dashingPower = 24f;
+    private float dashingTime = 0.2f;
+    private float dashingCooldown = 1f;
 
     // Start is called before the first frame update
     void Start()
@@ -22,6 +31,11 @@ public class PlayerMovementScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (isDashing)
+        {
+            return;
+        }
+        
         // definicja kierunku i zapisywanie w logu
         direction = Input.GetAxis("Horizontal");
 
@@ -36,22 +50,41 @@ public class PlayerMovementScript : MonoBehaviour
             player.velocity = new Vector2(direction * speed, player.velocity.y);
             transform.localScale = new Vector2(0.8f, 0.8f);
         }
-
         else
         {
             player.velocity = new Vector2(0, player.velocity.y);
         }
 
-        // kod na poruszanie sie triggera wraz z postacia
-        fallDetector.transform.position = new Vector2(transform.position.x, fallDetector.transform.position.y);
+        // if na dash
+        if (Input.GetKeyDown(KeyCode.LeftShift) && canDash)
+        {
+            StartCoroutine(Dash());
         }
 
+        // kod na poruszanie sie triggera wraz z postacia
+        fallDetector.transform.position = new Vector2(transform.position.x, fallDetector.transform.position.y);
+    }
+
         // ify na trigger od spadania
-        private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Respawn")
         {
-            if (collision.tag == "Respawn")
-            {
-                transform.position = respawnPoint;
-            }
+            transform.position = respawnPoint;
+        }
+    }
+
+    private IEnumerator Dash()
+    {
+        canDash = false;
+        isDashing = true;
+        float originalGravity = player.gravityScale;
+        player.gravityScale = 0f;
+        player.velocity = new Vector2(transform.localScale.x * dashingPower, 0f);
+        yield return new WaitForSeconds(dashingTime);
+        player.gravityScale = originalGravity;
+        isDashing = false;
+        yield return new WaitForSeconds(dashingCooldown);
+        canDash = true;
     }
 }
