5be3d6267431bfeaa1c3c02998d3930931a1b616
Frankiplier <agatarucka@gmail.com>
2024-06-11 00:48:22 +0200

    Set up main menu and keyboard input

---
+++ Trash hunt/Assets/InputManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : MonoBehaviour
+{
+    public static InputManager instance;
+
+    public bool MenuOpenCloseInput { get; private set; }
+    private PlayerInput playerInput;
+    private InputAction menuOpenCloseAction;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+
+        playerInput = GetComponent<PlayerInput>();
+        menuOpenCloseAction = playerInput.actions["MenuOpenClose"];
+    }
+
+    private void Update()
+    {
+        MenuOpenCloseInput = menuOpenCloseAction.WasPressedThisFrame();   
+    }
+}
---
+++ Trash hunt/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] GameObject credits;
+
+
+    public void Start()
+    {
+        Time.timeScale = 0f;
+
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        credits.SetActive(false);
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadSceneAsync(1);
+    }
+
+    public void Credits()
+    {
+        credits.SetActive(true);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    void OnDisable()
+    {
+        Time.timeScale = 1f;
+    }
+}
